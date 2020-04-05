12cbfad6e4935eb5cea21da3cd79b0e71758c839
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2020-04-04 19:48:00 -0700

    Add files via upload

---
+++ Hangman/GetWords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman
+{
+    static class GetWord
+    {
+        public static string WordGetter()
+        {
+            string[] mySecretWords = { "Bananas", "Island", "homework", "Family", "hangman", "robot", "president",
+                                       "Crab", "stanley", "Harbor", "Noodles", "", "Learning", "Savages", "Teakwood",
+                                       "Chocolate", "Runner", "huskey", "Hiking", "Microsoft", "military"};
+            Random rand = new Random();
+            int index = rand.Next(mySecretWords.Length);
+            string word = mySecretWords[index];
+            // word = "Rachel";
+            return word;
+        }
+    }
+}
