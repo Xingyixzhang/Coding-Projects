6dc9f705dd4fcfa723dd50a38293c40691ee457e
Xingyi Zhang <59896882+Xingyixzhang@users.noreply.github.com>
2020-04-13 14:14:33 -0700

    Create Welcome.php

---
+++ Hikers/Welcome.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        Welcome <?php echo $_post["name"]; ?><br>
+        Your selected country is: <?php echo $_post["countryChoice"]; ?><br>
+        And your language preference is: <?php echo $_post["langChoice"]; ?><br>
+    </body>
+</html>
